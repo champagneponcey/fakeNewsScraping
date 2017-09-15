8cb0b192635705e1f49efa8b504f74e2d5ccaadc
Jess <jsscthomp@gmail.com>
2017-09-15 12:38:00 -0400

    setting up mongoosein Article.js

---
+++ model/Article.js
@@ -0,0 +1,30 @@
+// require mongoose
+var mongoose = require("mongoose");
+
+// creating Schema class
+var Schema = mongoose.Schema;
+
+// creates article schema
+var ArticleSchema = new Schema({
+        // requires a title
+        title: {
+            type: String,
+            require: true
+        },
+        // requires a link
+        link: {
+            type: String,
+            required: true
+        },
+        // referring to note model
+        note: {
+            type: Schema.Types.ObjectId,
+            ref: "Note"
+        }
+    }):
+
+    // create article model with the ArticleSchema
+    var Article = mongoose.model("Article");
+
+// export model
+module.exports = Article;
