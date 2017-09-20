b1b4e1709017afe1eacf4194337b98390d27fc36
Jess <jsscthomp@gmail.com>
2017-09-20 11:24:51 -0400

    updated server

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const request = require("request");
 // set mongoose with JS ES6 Promises
 mongoose.Promise = Promise;
 
+PORT = process.env.PORT || 3333;
 // initialize Express
 var app = express();
 
@@ -30,7 +31,7 @@ app.use(express.static("public"));
 
 // Database config for mongoose
 // db: beautyblog
-mongoose.connect("mongodb://localhost/beautyblog", { useMongoClient: true });
+mongoose.connect("mongodb://heroku_nh1m14t6:gmj8kt0oqb44lirl3guvqoffn1@ds133104.mlab.com:33104/heroku_nh1m14t6", { useMongoClient: true });
 // hook mongoose connection to db
 var db = mongoose.connection;
 
@@ -48,3 +49,92 @@ console.log("\n**********************************\n" +
     "Grabbing every article name and link\n" +
     "from Byrdie's main page: " +
     "\n**********************************\n");
+
+app.listen(PORT, function() {
+    console.log("App listening on port: " + PORT)
+});
+
+
+// Routes
+// =============================================
+
+// initialize express
+var app = express();
+
+app.get("/", function(req, res) {
+        res.sendFile("index");
+    })
+    // a GET request to scrape website
+app.get("/scrape", function(req, res) {
+    // grab body of html with request
+    request("http://www.byrdie.com/section/skin", function(error, response, html) {
+        // load cheerio and save to $ for easy shorthand
+        var $ = cheerio.load(html);
+
+        // Grab every promo-feed-img within div tag
+        $("div.promo-feed-img").each(function(i, element) {
+            // save empty result object
+            var results = {};
+
+            // add text and href of everylink and save them to result object
+            results.title = $(this).children("a").text();
+            results.link = $(this).children("a").attr("href");
+            results.imgLink = $(this).children("a").find("img").attr("scr");
+
+            // using article model, create a new entry
+            var entry = new Article(results);
+
+            // save new entry to db
+            entry.save(function(err, doc) {
+                // log error if any
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(doc);
+                }
+            });
+        });
+    });
+    // tell browser scraping finished
+    res.send("Scrape Complete");
+});
+
+// will get scraped articles from DB
+app.get("/articles", function(req, res) {
+    // grab every article from db
+    Article.find({}, function(error, doc) {
+        // log any errors
+        if (error) {
+            console.log(error);
+        } else {
+            res.json(doc);
+        }
+    });
+});
+
+// create/replace notes for articles
+app.post("/articles/:id", function(req, res) {
+    // create new note and pass the req.body to entry
+    var newNote = new Note(req.body);
+
+    // save new note to db
+    newNote.save(function(error, doc) {
+        // log any errors
+        if (error) {
+            console.log(error);
+        } else {
+            // use article id to find and update note
+            Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+                // execute query
+                .exec(function(err, doc) {
+                    // log any errors
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        // or send document to browser
+                        res.send(doc);
+                    }
+                });
+        }
+    });
+});
