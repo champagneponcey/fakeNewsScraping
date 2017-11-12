db27afa4f0e0c2a168575fdd28a4ec98d449f3dc
Jess <jsscthomp@gmail.com>
2017-11-12 11:23:27 -0500

    finished scrape

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -20,19 +20,37 @@ var scrape = function(cb) {
             
         // Then we grab the inner text of the this element and store it
         // to the head variable. This is the article headline
-        var head = $(this).children("a").find("img").attr("alt");
+        var title = $(this).children("a").find("img").attr("alt");
         var link = $(this).children("a").attr("href");
         var img = $(this).children("a").find("img").attr("scr");
 
         // if head/link/img are all there then do the following
-        if (head && link && img) {
+        if (title && link && img) {
             // trim function to tidy our headlines and summaries
             // remove extra lines, extra spacing, extra tabs, etc.. to increase to typographical cleanliness.
-            var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-            var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+            var titleNeat = title.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+            var linkNeat = link.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
 
+            // initialize object to push to article array
+            var addData = {
+                title: titleNeat,
+                link: linkNeat,
+                img: img
+            };
+
+            results.push(addData);
         }
-}
+    });
+
+    // send array of article to cb function
+    cb(results);
+});
+
+// export function
+module.exports = scrape;
+
+
+
 
 
 
